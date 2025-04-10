d253c16b1a906031dd6a1a7ee3637c59f727ca65
asalako <augustin-semevo.salako@etudiant.univ-rennes.fr>
2025-04-10 16:38:10 +0200

    Modification de la méthode createEvent dans event.service.ts pour gérer les dates coté front

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -11,9 +11,27 @@ export class EventService {
 
     constructor(private http: HttpClient) {}
 
+    //createEvent(event: Event): Observable<Event> {
+
+        //  On formate la date
+     //   if (event.date instanceof Date) {
+            // On vérifie si la date contient 'Z' et on le retire si nécessaire
+       //     event.date = event.date.toISOString().slice(0, 19);  // Enlève le 'Z' si présent
+      //  }
+       // return this.http.post<Event>(`${this.apiUrl}/create`, event);
+   // }
+
     createEvent(event: Event): Observable<Event> {
+        if (event.date instanceof Date) {
+            // Convertir l'objet Date en chaîne de caractères au format ISO sans 'Z'
+            const dateString = event.date.toISOString().slice(0, 19); // Retirer 'Z' du format ISO
+            // On assigne la chaîne de caractères au champ 'date' avant de l'envoyer
+            event.date = dateString as any; // Forcer le type pour contourner le problème
+        }
+    
         return this.http.post<Event>(`${this.apiUrl}/create`, event);
     }
+    
 
     getAllEvents(): Observable<Event[]> {
         return this.http.get<Event[]>(`${this.apiUrl}/all`);
