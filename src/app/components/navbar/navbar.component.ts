00aa4166e71fa94fc1a09fb12c5d78b0325ae3a2
asalako <augustin-semevo.salako@etudiant.univ-rennes.fr>
2025-04-12 04:53:11 +0200

    Ajout d'une navBar basique pour naviguer entre les menis de l'app

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  imports: [CommonModule, RouterModule]
+})
+export class NavbarComponent {
+  isMenuOpen = false;
+}
