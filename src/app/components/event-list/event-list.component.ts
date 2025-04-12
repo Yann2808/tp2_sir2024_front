1aa0df2c14f2e9ac485437876da1a456302f77ea
asalako <augustin-semevo.salako@etudiant.univ-rennes.fr>
2025-04-12 15:00:50 +0200

    ajout de tailwind css

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { Event } from '../../models/event.model';
+import { EventService } from '../../services/event.service';
 
 @Component({
   selector: 'app-event-list',
-  imports: [],
+  imports: [TableModule],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.scss'
 })
 export class EventListComponent {
+  events: Event[] = [];
 
+  constructor(private eventService: EventService) {}
+
+  ngOnInit() {
+    // On appelle la méthode du service pour récupérer les évènements depuis l'API
+    this.eventService.getAllEvents().subscribe ({
+      next: (data) => {
+        this.events = data;
+      },
+      error: (err) => {
+        console.error('Erreur lors de la récupération des évènelents',err);
+      }
+
+      // possible d'ajouter une toast en rouge au lieu de la console
+    });
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,ts}", // Ajustez selon les fichiers de votre projet Angular
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
