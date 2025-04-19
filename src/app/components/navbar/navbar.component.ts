eef9d77fecc3f0c00583c512de0fd892dc284e03
asalako <augustin-semevo.salako@etudiant.univ-rennes.fr>
2025-04-19 22:49:50 +0200

    Création d'une navBar avec Bootstrap

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, ButtonModule]
 })
 export class NavbarComponent {
   isMenuOpen = false;
