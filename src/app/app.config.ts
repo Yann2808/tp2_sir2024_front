021c00f0605963a7381c780fd117c0b002c7e497
asalako <augustin-semevo.salako@etudiant.univ-rennes.fr>
2025-04-30 00:39:11 +0200

    Implémentation d'un système d'authentification avec Basic Auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,14 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
+    provideHttpClient(),
     provideAnimationsAsync(),
     provideClientHydration(withEventReplay())]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+/// <reference types="@angular/localize" />
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AuthInterceptor } from './app/auth.interceptor';
+import { AuthService } from './app/services/auth/auth.service';
 
 //bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
 
@@ -13,6 +17,9 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideAnimationsAsync(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
+    AuthInterceptor
   ]
 }).catch(err => console.error(err));
